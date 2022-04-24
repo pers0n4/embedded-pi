7ca1ebd351906a549ce81453106fd40afdcbcad3
Dong-Young Kim <31337.persona@gmail.com>
2022-04-25 00:32:23 +0900

    feat: add dht22 test script

---
+++ dht22/dht22test.py
@@ -0,0 +1,24 @@
+import time
+
+import adafruit_dht
+import board
+
+device = adafruit_dht.DHT22(board.D18)
+
+
+while True:
+    try:
+        temperature = device.temperature
+        humidity = device.humidity
+        print("Temp: {temperature:.1f}°C, Humidity: {humidity}% ")
+
+    except RuntimeError as error:
+        # Errors happen fairly often, DHT's are hard to read, just keep going
+        print(error.args[0])
+        time.sleep(1.0)
+        continue
+    except Exception as error:
+        device.exit()
+        raise error
+
+    time.sleep(1.0)
